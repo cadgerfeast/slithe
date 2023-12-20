d090d2091d44faf35831d5868f1aed5cb84fd688
cadgerfeast <cadgerfeast@users.noreply.github.com>
2023-12-20 17:21:31 +0000

    deploy: 92a3b47d624f8b0bb7d5820cef583b531507a741

--- assets/chunks/api.XBP9EDJC.js
+++
@@ -1,7 +0,0 @@
-import{Y as t}from"./framework.xdav9VQu.js";async function a(i){const{addNotification:o}=await t(()=>import("./index.k-h2XGDI.js"),__vite__mapDeps([0,1]));o({type:"info",icon:{name:"info",size:"16px"},message:i})}export{a as n};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/chunks/index.k-h2XGDI.js","assets/chunks/sl-layout.oki_j8Kt.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
