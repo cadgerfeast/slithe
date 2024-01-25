7d3d060c13369f040b8461ec1db8ef64561658e5
cadgerfeast <cadgerfeast@users.noreply.github.com>
2024-01-25 10:41:13 +0000

    deploy: 1631ebb5debaacc7d292e39c35cdff90efa06912

--- assets/chunks/api.6nLvCsr2.js
+++
@@ -1,7 +0,0 @@
-import{X as t}from"./framework.bINXxMeB.js";async function a(i){const{addNotification:o}=await t(()=>import("./index.7sbXo6Vq.js"),__vite__mapDeps([0,1]));o({type:"info",icon:{name:"info",size:"16px"},message:i})}export{a as n};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/chunks/index.7sbXo6Vq.js","assets/chunks/sl-layout.zElb8nfO.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/chunks/api.KdWWsuXV.js
@@ -0,0 +1,7 @@
+import{X as t}from"./framework.bINXxMeB.js";async function a(i){const{addNotification:o}=await t(()=>import("./index.pb6l77Tq.js"),__vite__mapDeps([0,1]));o({type:"info",icon:{name:"info",size:"16px"},message:i})}export{a as n};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/chunks/index.pb6l77Tq.js","assets/chunks/sl-splitter2.aAx8eQ1B.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
