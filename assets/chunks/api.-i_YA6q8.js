6f1afebffb593ec7b9d44fad630025faa36c7389
cadgerfeast <cadgerfeast@users.noreply.github.com>
2024-03-20 14:09:54 +0000

    deploy: 5a378ea3e81a814467b51c3c837fc66339d0ec1d

---
+++ assets/chunks/api.-i_YA6q8.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/chunks/index.6B12NNhu.js","assets/chunks/sl-splitter2.SV8WbDoe.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{X as t}from"./framework.bINXxMeB.js";async function a(i){const{addNotification:o}=await t(()=>import("./index.6B12NNhu.js"),__vite__mapDeps([0,1]));o({type:"info",icon:{name:"info",size:"16px"},message:i})}export{a as n};
